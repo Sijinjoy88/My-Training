4c588e1b2dacb2d3cb923108e2ad6a2cfd9a08e2
Titanium Setup <Titanium Setup@10.0.0.12>
2022-05-30 22:05:56 +1000

    New change

--- Maven Project/MavenProjectSample/src/main/java/obscura/MavenProjectSample/App.java
+++ Maven Project/MavenProjectSample/src/main/java/obscura/MavenProjectSample/App.java
@@ -11,10 +11,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
 	
